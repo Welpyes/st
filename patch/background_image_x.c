566f8d384b8ed9e0d160ad997a8707d350df201b
Welpyes <helloilikeoppai@protonmail.com>
2026-01-14 21:46:01 +0800

    added support background image for mainstream formats

--- patch/background_image_x.c
+++ patch/background_image_x.c
@@ -6,78 +6,58 @@ updatexy()
 }
 
 /*
- * load farbfeld file to XImage
- */
-XImage*
-loadff(const char *filename)
-{
-	uint32_t i, hdr[4], w, h, size;
-	uint64_t *data;
-	FILE *f = fopen(filename, "rb");
-
-	if (f == NULL) {
-		fprintf(stderr, "could not load background image.\n");
-		return NULL;
-	}
-
-	if (fread(hdr, sizeof(*hdr), LEN(hdr), f) != LEN(hdr)) {
-		fprintf(stderr, "fread: %s\n", ferror(f) ? "" : "Unexpected end of file reading header");
-		fclose(f);
-		return NULL;
-	}
-
-	if (memcmp("farbfeld", hdr, sizeof("farbfeld") - 1)) {
-		fprintf(stderr, "Invalid magic value\n");
-		fclose(f);
-		return NULL;
-	}
-
-	w = ntohl(hdr[2]);
-	h = ntohl(hdr[3]);
-	size = w * h;
-	data = xmalloc(size * sizeof(uint64_t));
-
-	if (fread(data, sizeof(uint64_t), size, f) != size) {
-		fprintf(stderr, "fread: %s\n", ferror(f) ? "" : "Unexpected end of file reading data");
-		fclose(f);
-		return NULL;
-	}
-
-	fclose(f);
-
-	for (i = 0; i < size; i++)
-		 data[i] = (data[i] & 0x00000000000000FF) << 16 |
-		           (data[i] & 0x0000000000FF0000) >> 8  |
-		           (data[i] & 0x000000FF00000000) >> 32 |
-		           (data[i] & 0x00FF000000000000) >> 24;
-
-	XImage *xi = XCreateImage(xw.dpy, DefaultVisual(xw.dpy, xw.scr),
-		DefaultDepth(xw.dpy, xw.scr), ZPixmap, 0,
-		(char *)data, w, h, 32, w * 8);
-	xi->bits_per_pixel = 64;
-	return xi;
-}
-
-/*
  * initialize background image
  */
 void
 bginit()
 {
 	XGCValues gcvalues;
-	Drawable bgimg;
-	XImage *bgxi = loadff(bgfile);
+	Imlib_Image img;
+	char path[PATH_MAX];
 
 	memset(&gcvalues, 0, sizeof(gcvalues));
 	xw.bggc = XCreateGC(xw.dpy, xw.win, 0, &gcvalues);
-	if (!bgxi)
+
+	if (bgfile[0] == '~') {
+		const char *home = getenv("HOME");
+		if (home) {
+			int ret = snprintf(path, sizeof(path), "%s%s", home, bgfile + 1);
+			if (ret < 0 || (size_t)ret >= sizeof(path)) {
+				fprintf(stderr, "path too long\n");
+				return;
+			}
+		} else {
+			strncpy(path, bgfile, sizeof(path));
+			path[sizeof(path)-1] = '\0';
+		}
+	} else {
+		strncpy(path, bgfile, sizeof(path));
+		path[sizeof(path)-1] = '\0';
+	}
+
+	img = imlib_load_image(path);
+	if (!img) {
+		fprintf(stderr, "could not load background image: %s\n", path);
 		return;
-	bgimg = XCreatePixmap(xw.dpy, xw.win, bgxi->width, bgxi->height,
-		DefaultDepth(xw.dpy, xw.scr));
-	XPutImage(xw.dpy, bgimg, dc.gc, bgxi, 0, 0, 0, 0, bgxi->width, bgxi->height);
-	XDestroyImage(bgxi);
+	}
+
+	imlib_context_set_image(img);
+	int w = imlib_image_get_width();
+	int h = imlib_image_get_height();
+
+	Pixmap bgimg = XCreatePixmap(xw.dpy, xw.win, w, h, DefaultDepth(xw.dpy, xw.scr));
+
+	imlib_context_set_display(xw.dpy);
+	imlib_context_set_visual(DefaultVisual(xw.dpy, xw.scr));
+	imlib_context_set_colormap(DefaultColormap(xw.dpy, xw.scr));
+	imlib_context_set_drawable(bgimg);
+
+	imlib_render_image_on_drawable(0, 0);
+	imlib_free_image();
+
 	XSetTile(xw.dpy, xw.bggc, bgimg);
 	XSetFillStyle(xw.dpy, xw.bggc, FillTiled);
+
 	if (pseudotransparency) {
 		updatexy();
 		MODBIT(xw.attrs.event_mask, 1, PropertyChangeMask);
--- patch/background_image_x.h
+++ patch/background_image_x.h
@@ -1,6 +1,3 @@
-#include <arpa/inet.h>
-
 static void updatexy(void);
-static XImage *loadff(const char *);
 static void bginit();
 static void reload_image();
