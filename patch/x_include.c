888ed240bcc4f227685de29a83cb12da5a42a83e
Welpyes <helloilikeoppai@protonmail.com>
2026-02-06 21:57:41 +0800

    initial commit

---
+++ patch/x_include.c
@@ -0,0 +1,7 @@
+/* Patches */
+#include "background_image_x.c"
+#include "boxdraw.c"
+#include "fixkeyboardinput.c"
+#include "font2.c"
+#include "fullscreen_x.c"
+#include "netwmicon.c"
---
+++ patch/x_include.h
@@ -0,0 +1,6 @@
+/* Patches */
+#include "background_image_x.h"
+#include "boxdraw.h"
+#include "font2.h"
+#include "fullscreen_x.h"
+#include "netwmicon.h"
